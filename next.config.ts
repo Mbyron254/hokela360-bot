09239dc819631c7ea92fc2ee145a581174c1df6e
Mbyron254 <byron@hokela.co.ke>
2024-10-27 14:05:35 +0300

    Lint Fixes

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: { // [!code ++]
-    serverComponentsExternalPackages: ['grammy'], // [!code ++]
-  }, // [!code ++]
+  serverExternalPackages: ['grammy'], // <-- Updated key here
 };
 
 export default nextConfig;
